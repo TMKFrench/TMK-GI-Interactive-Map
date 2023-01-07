44dd68e5abcddf760930ead0e43cf487478d4a7d
Laurent <tmkfrench@gmail.com>
2023-01-08 00:57:05 +0100

    Correction gestion Cbx popup Devmap
    Correction de la gestion des cbx dans les popup de la Devmap.

--- assets/js/devsum.js
+++ assets/js/devsum.js
@@ -5,7 +5,7 @@ function onMapClick(e) {
     var x = Math.floor(txt.x);
     var y = Math.floor(txt.y);
     var under = false;
-    L.marker([e.latlng.lat, e.latlng.lng], {uid: uid, type: markertype, icon: window[markertype+'Icon'], title: "Id: "+uid, under: under}).bindPopup('<input type="text" value="['+x+','+y+']" class="py-2 px-4 border rounded text-xs w-full text-center" onclick="select()" /><br><span class="py-2 px-4 text-xs w-full text-center"> UID : '+uid+' MID : '+mid+'</span><br><span class="py-2 px-4 text-xs w-full text-center"><input class="under-point" type="checkbox" >Under</span><br /><a class="delete-point underline mt-2 font-bold inline-block" style="color:red!important;" href="#!">Supprimer</a>', {maxHeight : 350, minWidth : 350}).on('click', updateCurrentMarker).addTo(map);
+    L.marker([e.latlng.lat, e.latlng.lng], {uid: uid, type: markertype, icon: window[markertype+'Icon'], title: "Id: "+uid, under: under}).bindPopup('<input type="text" value="['+x+','+y+']" class="py-2 px-4 border rounded text-xs w-full text-center" onclick="select()" /><br><span class="py-2 px-4 text-xs w-full text-center"> UID : '+uid+' MID : '+mid+'</span><br><span class="py-2 px-4 text-xs w-full text-center"><input class="under-point" type="checkbox" id="devbox" data-cbxid="'+uid+'" /> Under</span><br /><a class="delete-point underline mt-2 font-bold inline-block" style="color:red!important;" href="#!">Supprimer</a>', {maxHeight : 350, minWidth : 350}).on('click', updateCurrentMarker).addTo(map);
     countmarker[""+markertype] += 1;
     userMarkers.push(uid);
     var datam = [uid, mid, markertype, x, y, under];
@@ -41,6 +41,16 @@ function updateCurrentMarker() {
     currentMarker = this;
 }
 
+function popUpOpen(e) {
+    var content = e.popup.getContent();
+
+    if($(content).find('input#devbox').length > 0) {
+      if(underMarkers.indexOf( $(content).find('input#devbox').first().data('cbxid') ) >= 0) {
+        $('input#devbox[data-cbxid="'+$(content).find('input#devbox').first().data('cbxid')+'"]').prop('checked', 'checked');
+      }
+    }
+}
+
 var currentMarker;
 var userMarkers = [];
 var markertype = 'ferblanc';
@@ -51,6 +61,7 @@ var mgroup = [
     'viparyas','noixajilenakh','scarabee','quandong','anguille'
 ]; // ,'aranara','aranara2','kalpalotus'
 var DBMarkers = L.layerGroup();
+var underMarkers = [];
 
 // Initialisation de la carte
 var map = new L.Map('devmap', {
@@ -79,7 +90,7 @@ function initmarkers() {
     // Depuis le fichiers JS
 
     mgroup.forEach(function(group) {
-        console.log(group);
+        // console.log(group);
         // console.table(window['list'+group]);
         if (window['list'+group]) {
             window['list'+group].forEach(function(marker) {
@@ -97,11 +108,14 @@ function initmarkers() {
 
     $.get('api/dev/import', function(res) {
         res.forEach(function(marker) {
-			console.log(marker.under)
+			// console.log(marker.under)
             newmarker = L.marker(unproject([marker.x, marker.y]), {uid: marker.uid, mid: marker.mid, icon: window[marker.mgroup+'Icon'], title: "Id: "+marker.uid, under: marker.under})
-            .bindPopup('<input type="text" value="['+marker.x+','+marker.y+']" class="py-2 px-4 border rounded text-xs w-full text-center" onclick="select()" /><br><span class="py-2 px-4 text-xs w-full text-center"> UID : '+marker.uid+' MID : '+marker.mid+'</span><br><span class="py-2 px-4 text-xs w-full text-center"><input class="under-point" type="checkbox" '+((marker.under)?"checked='checked'":"")+'>Under</span><br /><a class="delete-point underline mt-2 font-bold inline-block" style="color:red!important;" href="#!">Supprimer</a>', {maxHeight : 350, minWidth : 350})
+            .bindPopup('<input type="text" value="['+marker.x+','+marker.y+']" class="py-2 px-4 border rounded text-xs w-full text-center" onclick="select()" /><br><span class="py-2 px-4 text-xs w-full text-center"> UID : '+marker.uid+' MID : '+marker.mid+'</span><br><span class="py-2 px-4 text-xs w-full text-center"><input class="under-point" type="checkbox" id="devbox" data-cbxid="'+marker.uid+'" /> Under</span><br /><a class="delete-point underline mt-2 font-bold inline-block" style="color:red!important;" href="#!">Supprimer</a>', {maxHeight : 350, minWidth : 350})
             .on('click', updateCurrentMarker).addTo(map);
             userMarkers.push(marker.uid);
+            if (marker.under) {
+                underMarkers.push(marker.uid);
+            }
             countmarker[""+marker.mgroup] += 1;
             console.log("groupe : "+marker.mgroup+", Nombre : "+countmarker[""+marker.mgroup]);
         });
@@ -114,6 +128,7 @@ function initmarkers() {
 
 // Créer un marqueur au clic
 map.on('click', onMapClick);
+map.on('popupopen', popUpOpen);
 
 $(document).ready(function() {
 
@@ -145,7 +160,12 @@ $(document).ready(function() {
 			if (typeof(res.ok) != "undefined") {
 				console.log(res.ok)
 				currentMarker.options.under = (currentMarker.options.under)?false:true;
-				console.log(currentMarker.getElement());
+                if (currentMarker.options.under) {
+                    if (underMarkers.indexOf(currentMarker.options.uid) <0) {underMarkers.push(currentMarker.options.uid)};
+                } else {
+                    underMarkers.splice(underMarkers.indexOf(currentMarker.options.uid), 1);
+                }
+				// console.log(currentMarker.getElement());
 			} else if (typeof(res.error) != "undefined")
 				alert("error "+res.error)
 			else
